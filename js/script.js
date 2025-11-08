99cc097675f9062aa9c81d2f7750daa43a6aa0b8
neobte <neobte@gmail.com>
2025-11-07 22:37:49 -0300

    Updating files

--- js/script.js
+++ js/script.js
@@ -2,34 +2,38 @@
 
 const d = document;
 
-const songs = [
-    { id: 1, artist: "Eddie Santiago", title: "Antidoto y veneno", src: "audio/salsa/Eddie Santiago - Antidoto y veneno.m4a", duration: 318.453333 },
-    { id: 2, artist: "La inmensidad", title: "Ay que amor", src: "audio/salsa/La inmensidad - Ay que amor.mp3", duration: 234.053333 },
-    { id: 3, artist: "La Misma Gente", title: "Llego el final", src: "audio/salsa/La Misma Gente - Llego el final.m4a", duration: 343.562448 },
-    { id: 4, artist: "Los Hermanos Moreno", title: "Por alguien como tu", src: "audio/salsa/Los Hermanos Moreno - Por alguien como tu.m4a", duration: 215.073174 },
-    { id: 5, artist: "Nino Segarra", title: "Como amigos si, como amantes no", src: "audio/salsa/Nino Segarra - Como amigos si, como amantes no.mp3", duration: 286.30204 },
-    { id: 6, artist: "Nino Segarra", title: "Entre la espada y la pared", src: "audio/salsa/Nino Segarra - Entre la espada y la pared.mp3", duration: 344.166167 },
-    { id: 7, artist: "Nino Segarra", title: "Porque te amo", src: "audio/salsa/Nino Segarra - Porque te amo.m4a", duration: 306.96 },
-    { id: 8, artist: "Paquito Guzman", title: "Que voy a hacer sin ti", src: "audio/salsa/Paquito Guzman - Que voy a hacer sin ti.m4a", duration: 332.644331 },
-    { id: 9, artist: "Pedro Arroyo", title: "Todo me huele a ti", src: "audio/salsa/Pedro Arroyo - Todo me huele a ti.mp3", duration: 322.08399 },
-    { id: 10, artist: "The New York Band", title: "Nadie como tu", src: "audio/salsa/The New York Band - Nadie como tu.m4a", duration: 287.370158 },
-    { id: 11, artist: "Willie Gonzalez", title: "Hazme olvidarla", src: "audio/salsa/Willie Gonzalez - Hazme olvidarla.mp3", duration: 336.133333 },
-    { id: 12, artist: "Johnny Rojas", title: "Adicto a ti", src: "audio/salsa/Johnny Rojas - Adicto a ti.m4a", duration: 326.008117 },
-    { id: 13, artist: "Lalo Rodriguez", title: "Ven devorame otra vez", src: "audio/salsa/Lalo Rodriguez - Ven devorame otra vez.m4a", duration: 307.617959 },
-    { id: 14, artist: "Frankie Ruiz", title: "Tu con el", src: "audio/salsa/Frankie Ruiz - Tu con el.m4a", duration: 303.508027 },
-    { id: 15, artist: "David Pabon", title: "Aquel viejo motel", src: "audio/salsa/David Pabon - Aquel viejo motel.m4a", duration: 308.866666 },
-    { id: 16, artist: "La Misma Gente", title: "Tu y yo", src: "audio/salsa/La Misma Gente - Tu y yo.m4a", duration: 282.331428 },
-    { id: 17, artist: "La Misma Gente", title: "Llegaste tu", src: "audio/salsa/La Misma Gente - Llegaste tu.m4a", duration: 266.631836 },
-    { id: 18, artist: "El Gran Combo", title: "Amame", src: "audio/salsa/El Gran Combo - Amame.mp3", duration: 324.7607 },
-    { id: 19, artist: "Hector Lavoe", title: "Siento", src: "audio/salsa/Hector Lavoe - Siento.mp3", duration: 395.420833 },
-    { id: 20, artist: "3-2 Get Funky", title: "Si ya no estas", src: "audio/salsa/3-2 Get Funky - Si ya no estas.m4a", duration: 268.608435 },
-];
+// const songList = [
+//     { id: 1, artist: "Eddie Santiago", title: "Antidoto y veneno", src: "audio/salsa/Eddie Santiago - Antidoto y veneno.m4a", duration: 318.453333 },
+//     { id: 2, artist: "La inmensidad", title: "Ay que amor", src: "audio/salsa/La inmensidad - Ay que amor.mp3", duration: 234.053333 },
+//     { id: 3, artist: "La Misma Gente", title: "Llego el final", src: "audio/salsa/La Misma Gente - Llego el final.m4a", duration: 343.562448 },
+//     { id: 4, artist: "Los Hermanos Moreno", title: "Por alguien como tu", src: "audio/salsa/Los Hermanos Moreno - Por alguien como tu.m4a", duration: 215.073174 },
+//     { id: 5, artist: "Nino Segarra", title: "Como amigos si, como amantes no", src: "audio/salsa/Nino Segarra - Como amigos si, como amantes no.mp3", duration: 286.30204 },
+//     { id: 6, artist: "Nino Segarra", title: "Entre la espada y la pared", src: "audio/salsa/Nino Segarra - Entre la espada y la pared.mp3", duration: 344.166167 },
+//     { id: 7, artist: "Nino Segarra", title: "Porque te amo", src: "audio/salsa/Nino Segarra - Porque te amo.m4a", duration: 306.96 },
+//     { id: 8, artist: "Paquito Guzman", title: "Que voy a hacer sin ti", src: "audio/salsa/Paquito Guzman - Que voy a hacer sin ti.m4a", duration: 332.644331 },
+//     { id: 9, artist: "Pedro Arroyo", title: "Todo me huele a ti", src: "audio/salsa/Pedro Arroyo - Todo me huele a ti.mp3", duration: 322.08399 },
+//     { id: 10, artist: "The New York Band", title: "Nadie como tu", src: "audio/salsa/The New York Band - Nadie como tu.m4a", duration: 287.370158 },
+//     { id: 11, artist: "Willie Gonzalez", title: "Hazme olvidarla", src: "audio/salsa/Willie Gonzalez - Hazme olvidarla.mp3", duration: 336.133333 },
+//     { id: 12, artist: "Johnny Rojas", title: "Adicto a ti", src: "audio/salsa/Johnny Rojas - Adicto a ti.m4a", duration: 326.008117 },
+//     { id: 13, artist: "Lalo Rodriguez", title: "Ven devorame otra vez", src: "audio/salsa/Lalo Rodriguez - Ven devorame otra vez.m4a", duration: 307.617959 },
+//     { id: 14, artist: "Frankie Ruiz", title: "Tu con el", src: "audio/salsa/Frankie Ruiz - Tu con el.m4a", duration: 303.508027 },
+//     { id: 15, artist: "David Pabon", title: "Aquel viejo motel", src: "audio/salsa/David Pabon - Aquel viejo motel.m4a", duration: 308.866666 },
+//     { id: 16, artist: "La Misma Gente", title: "Tu y yo", src: "audio/salsa/La Misma Gente - Tu y yo.m4a", duration: 282.331428 },
+//     { id: 17, artist: "La Misma Gente", title: "Llegaste tu", src: "audio/salsa/La Misma Gente - Llegaste tu.m4a", duration: 266.631836 },
+//     { id: 18, artist: "El Gran Combo", title: "Amame", src: "audio/salsa/El Gran Combo - Amame.mp3", duration: 324.7607 },
+//     { id: 19, artist: "Hector Lavoe", title: "Siento", src: "audio/salsa/Hector Lavoe - Siento.mp3", duration: 395.420833 },
+//     { id: 20, artist: "3-2 Get Funky", title: "Si ya no estas", src: "audio/salsa/3-2 Get Funky - Si ya no estas.m4a", duration: 268.608435 },
+// ];
 
-const totalSongs = songs.length;
+// const totalSongs = songList.length;
 // Ordenamos el listado de canciones por artista
-songs.sort((a, b) => a.artist > b.artist ? 1 : a.artist < b.artist ? -1 : 0);
+// songList.sort((a, b) => a.artist > b.artist ? 1 : a.artist < b.artist ? -1 : 0);
 
-let songsCopy = [...songs];
+// let songListCopy = [...songList];
+
+let URL = `https://neobte.github.io/music-app/audio/salsa/playlist.json`;
+
+let songList, songListCopy, totalSongs;
 
 const audioPlayer = new Audio();
 
@@ -70,21 +74,38 @@ d.addEventListener("DOMContentLoaded", () => {
 });
 
 const init = () => {
-    // 1. Cargamos el indice cero
-    loadSong(songsCopy[index]);
-    // 2. Mostramos la lista de canciones
-    displayRows(songsCopy);
+
+    sendHttpRequest("GET", URL, null, handleResponse);
+
     // 3. Reseteamos los valores
     volumeSlider.value = 100;
     audioCurrentTimeSlider.value = 0;
     dataTable.parentElement.scrollTop = 0;
 }
 
+const handleResponse = response => {
+
+    songList = JSON.parse(response);
+
+    songListCopy = [...songList];
+
+    totalSongs = songListCopy.length;
+
+    if (index < totalSongs) {
+        // 1. Cargamos el indice cero
+        loadSong(songListCopy[index]);
+    }
+    // 2. Mostramos la lista de canciones
+    displayRows(songListCopy);
+}
+
 const loadSong = song => {
-    audioPlayer.src = song.src;
+    audioPlayer.src = `https://neobte.github.io/music-app/audio/salsa/${song.name}`;
     audioPlayer.dataset.songId = song.id;
     audioPlayer.load();
-    songTitle.textContent = `🎵 ${song.artist} - ${song.title} 🎵`;
+
+    const [artist, title] = song.name.slice(0, song.name.lastIndexOf('.')).split(" - ");
+    songTitle.textContent = `🎵 ${artist} - ${title} 🎵`;
 
     if (songTitle.scrollWidth > parentElementOffsetWidth) {
         songTitle.style.animation = "slideIn 16s linear infinite";
@@ -113,10 +134,10 @@ playPauseBtn.addEventListener("click", () => {
 forwardBtn.addEventListener("click", () => {
     index = (index + 1) % totalSongs;
     if (!audioPlayer.paused) {
-        loadSong(songsCopy[index]);
+        loadSong(songListCopy[index]);
         audioPlayer.play();
     } else {
-        loadSong(songsCopy[index]);
+        loadSong(songListCopy[index]);
     }
     removeCssClass("playing");
     addCssClass("playing");
@@ -134,10 +155,10 @@ backwardBtn.addEventListener("click", () => {
     else if (!isFirstClick || audioPlayer.currentTime === 0) {
         index = (index - 1 + totalSongs) % totalSongs;
         if (!audioPlayer.paused) {
-            loadSong(songsCopy[index]);
+            loadSong(songListCopy[index]);
             audioPlayer.play();
         } else {
-            loadSong(songsCopy[index]);
+            loadSong(songListCopy[index]);
         }
         isFirstClick = true;
     }
@@ -151,20 +172,20 @@ shuffleBtn.addEventListener("click", () => {
     shuffleBtn.style.color = isShuffle ? "#ff9800" : "currentColor";
     if (isShuffle) {
         // Encuentra el elemento pero no lo elimina del arreglo
-        // const element = songsCopy.find(song => song.id == audioPlayer.dataset["songId"]);
+        // const element = songListCopy .find(song => song.id == audioPlayer.dataset["songId"]);
         // Encuentra el elemento y lo extrae del arreglo
-        const currentlyPlayingSong = songsCopy.splice(songs.findIndex(song => song.id == audioPlayer.dataset['songId']), 1)[0];
+        const currentlyPlayingSong = songListCopy.splice(songListCopy.findIndex(song => song.id == audioPlayer.dataset['songId']), 1)[0];
         // Barajamos el arreglo sin la cancion actualmente en reproduccion
-        songsCopy = shuffle(songsCopy);
+        songListCopy = shuffle(songListCopy);
         // Enviamos al principio la canción que esta actualmente en reproducción despues de la mezcla
-        songsCopy.unshift(currentlyPlayingSong);
+        songListCopy.unshift(currentlyPlayingSong);
         // Seteamos el indice a cero correspondiente a la canción actualmente en reproduccion
         index = 0;
     } else {
         // Buscamos el índice en el array original
-        index = songs.findIndex(song => song.id == audioPlayer.dataset['songId']);
+        index = songList.findIndex(song => song.id == audioPlayer.dataset['songId']);
         // Devolvemos el orden del array original
-        songsCopy = [...songs];
+        songListCopy = [...songList];
     }
 
     if (index > -1) {
@@ -193,7 +214,7 @@ audioPlayer.addEventListener('ended', () => {
         return;
     }
     index++;
-    loadSong(songsCopy[index]);
+    loadSong(songListCopy[index]);
     audioPlayer.play();
 
     removeCssClass("playing");
@@ -209,7 +230,7 @@ const displayRows = (rows) => {
         // Create table cells (td) for each song property
         tr.innerHTML = `
                     <td>${idx + 1}.</td>
-                    <td>${song.artist} - ${song.title}</td>
+                    <td>${removeExtension(song.name)}</td>
                     <td>${formatTime(song.duration)}</td>`;
 
         if (idx === index && audioPlayer.src !== "") {
@@ -294,7 +315,7 @@ const removeCssClass = (cssClass) => {
 
 const container = d.getElementById("play-list");
 const addCssClass = (cssClass) => {
-    const elt = dataTable.querySelector(`tr[data-id="${songsCopy[index].id}"]`);
+    const elt = dataTable.querySelector(`tr[data-id="${songListCopy[index].id}"]`);
     if (elt) {
         elt.classList.add(cssClass);
         container.scrollTop = elt.offsetTop - (container.clientHeight / 2) + (elt.offsetHeight / 2);
@@ -331,4 +352,37 @@ const getRandomHexColor = (max = 256) => {
         ((Math.random() * max) | 0).toString(16).padStart(2, '0') +
         ((Math.random() * max) | 0).toString(16).padStart(2, '0') +
         ((Math.random() * max) | 0).toString(16).padStart(2, '0');
+}
+
+// Send request
+function sendHttpRequest(method, url, data, callback) {
+    const xhr = getXhr();
+    xhr.onreadystatechange = processRequest;
+    function getXhr() {
+        if (window.XMLHttpRequest) {
+            return new XMLHttpRequest();
+        } else {
+            return new ActiveXObject("Microsoft.XMLHTTP");
+        }
+    }
+    function processRequest() {
+        if (xhr.readyState == XMLHttpRequest.DONE) {
+            if (xhr.status == 200 && xhr.response != null) {
+                if (callback) callback(xhr.response);
+            } else {
+                console.log("There was a problem retrieving the data: " + xhr.statusText);
+            }
+        }
+    }
+    xhr.open(method, url + ((/\?/).test(url) ? "&" : "?") + (new Date()).getTime());
+    xhr.onloadstart = function (e) { /*openLoader();*/ }
+    xhr.onloadend = function (e) { /*closeLoader();*/ }
+    if (data && !(data instanceof FormData)) xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    xhr.send(data);
+    xhr.onerror = function (e) { console.log("Error: " + e + " Could not load url."); }
+}
+
+const removeExtension = name => {
+    // return name.slice(0, name.lastIndexOf("."));
+    return name.split(".")[0];
 }
